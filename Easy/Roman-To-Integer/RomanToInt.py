ceb0736b202942bb5cf996c7dcd98123706d3373
Brendon Murthum <bmmurthum@gmail.com>
2020-08-18 01:55:08 -0400

    Adding Initial RomanToInteger Files

---
+++ Easy/Roman-To-Integer/RomanToInt.py
@@ -0,0 +1,30 @@
+# Brendon Murthum. August 2020.
+#
+# Python 3
+# Leetcode Problem - Roman to Integer
+# https://leetcode.com/problems/roman-to-integer/
+#
+# GOAL: Roman numerals are represented by seven different symbols: I, V, X, L,
+#   C, D and M. For example, two is written as II in Roman numeral, just two
+#   one's added together. Twelve is written as, XII, which is simply X + II.
+#   The number twenty seven is written as XXVII, which is XX + V + II.
+#   Roman numerals are usually written largest to smallest from left to right.
+#   However, the numeral for four is not IIII. Instead, the number four is
+#   written as IV. Because the one is before the five we subtract it making
+#   four. The same principle applies to the number nine, which is written as
+#   IX. Given a roman numeral, convert it to an integer. Input is guaranteed
+#   to be within the range from 1 to 3999.
+#
+# OVERVIEW: Blah blah
+
+# Variety of test cases.
+test1 = "X"
+test2 = "IX"
+test3 = "III"
+
+# This function takes a string of a roman numeral and returns a valid integer.
+def romanToInt(s: str) -> int:
+
+    return
+
+print("Input: ", s, "Output: ", romanToInt(s))
