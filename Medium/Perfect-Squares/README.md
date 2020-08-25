628801c8d4249c996823c4cf475fe800ff6b2c78
Brendon Murthum <bmmurthum@gmail.com>
2020-08-24 22:49:50 -0400

    Initial Perfect Squares Setup

---
+++ Medium/Perfect-Squares/README.md
@@ -0,0 +1,60 @@
+# Perfect Squares
+
+**Description:**
+
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+**Example:**
+```
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+```
+
+**To Run:**
+
+To run this program, make sure you have Java. The solution is written in Java 1.0.8. You may have to confirm that Java is included in your `PATH` variables. Navigate to the directory containing the file `perfectSquares.java` and compile with the command `$ javac perfectSquares.java`.
+
+Run the program by calling Java within this directory on the main class:
+
+`$ java perfectSquares`
+
+## Overview
+
+## Reflections
+
+## Further Inquiry
+
+## Test Cases
+
+**Case 1:** A square.
+```
+Input: 4
+Output: 1  
+```
+
+**Case 2:** A number non-square.
+```
+Input: 3
+Output: 3
+Explanation: 1 + 1 + 1
+```
+
+**Case 3:** Zero.
+```
+Input: 0
+Output: 0
+```
+
+**Case 4:** Minimum of multiple answers.
+```
+Input: 12
+Output: 3
+Explanation: 4 + 4 + 4, not 9 + 1 + 1 + 1
+```
+
+**Case 5:** A number non-square.
+```
+Input: 13
+Output: 2
+```
--- Medium/Perfect-Squares/perfectSquares.java
+++ Medium/Perfect-Squares/perfectSquares.java
@@ -10,8 +10,56 @@
  * OVERVIEW:
  */
 
+
+// For use of sqrt() function.
+import java.lang.Math;
+
+
+/** To test the algorithm of finding sums of squares. */
 public class perfectSquares {
+
+    // Set the test cases.
+    static int[][] testCases = {
+        {0,0},      {16,1},     {100,1},    {232,2},
+        {1,1},      {27,3},     {101,2},    {277,2},
+        {2,2},      {29,2},     {102,3},    {384,3},
+        {3,3},      {34,2},     {103,4},    {572,4},
+        {4,1},      {37,2},     {104,2},    {733,2},
+        {5,2},      {50,2},     {115,3},    {799,4},
+        {6,3},      {58,2},     {154,3},    {1022,3},
+        {10,2},     {61,2},     {159,4},    {1337,3},
+        {12,3},     {62,3},     {173,2},    {2645,2},
+        {13,2},     {99,3},     {181,2},    {5655,4}
+    };
+
+    /** Runs the program. */
     public static void main(String[] args){
-        System.out.println("Hello world");
+        // Run the test cases.
+        testCase(testCases);
+        return;
+    }
+
+    /** Returns the minimum number of squares that can be summed to the given
+        number. */
+    public static int numSquares(int n) {
+        return 2;
+    }
+
+    /** Runs each test-case through the algorithm. */
+    public static void testCase(int[][] testList) {
+        int numberOfTests = testList.length;
+        int numberOfSuccesses = 0;
+        double percentSuccess;
+        for (int i = 0; i < numberOfTests; i++) {
+            if(numSquares(testList[i][0]) != testList[i][1]) {
+                System.out.println("FAILURE: " + testList[i][0] + " != " +
+                    numSquares(testList[i][0]) + ", should be: " +
+                    testList[i][1]);
+            } else {
+                numberOfSuccesses += 1;
+            }
+        }
+        percentSuccess = Math.floor(((double)numberOfSuccesses / (double)numberOfTests) * 100);
+        System.out.println(percentSuccess + "% Success " + numberOfSuccesses + "/" + numberOfTests + " Cases");
     }
 }
